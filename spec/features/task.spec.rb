6e1546932766b94963de13b29b4a19ac711c9ec8
KazukiSaito <kazoo@kazooPro.local>
2018-12-13 18:49:13 +0900

    :zap: ready to start feature test.

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "TASK management functionality", type: :feature do
 
+  scenario "task index test" do
+
+  end
+
+  scenario "create a new task" do
+
+  end
+
+  scenario "show specific task info" do
+
+  end
 end
