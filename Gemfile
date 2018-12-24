8469c2f557e1986e0b46d47a17682ce993b3139c
KazukiSaito <kazoo@kazooPro.local>
2018-12-24 19:35:28 +0900

    :building_construction: factory updated for the features.

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'spring-commands-rspec'
+  gem 'rails-erd'
 end
 
 # group :test do
--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -5,4 +5,23 @@ FactoryBot.define do
     content { 'task_content1' }
   end
 
+  factory :second_task do
+    title { 'task_title2' }
+    content { 'task_content2' }
+  end
+
+  factory :task3 do
+    title { 'task_title3' }
+    content { 'task_content3' }
+  end
+
+  factory :task4 do
+    title { 'task_title4' }
+    content { 'task_content4' }
+  end
+
+  factory :task5 do
+    title { 'task_title5' }
+    content { 'task_content5' }
+  end
 end
