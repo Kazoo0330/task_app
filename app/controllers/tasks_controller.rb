80d82859b42e6b66be11730a925b58c8b9b8b9c3
KazukiSaito <kazoo@kazooPro.local>
2019-01-30 22:29:38 +0900

    :children_crossing: setting for task with current_user/restriction of other's profile editing.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,17 +2,15 @@ class TasksController < ApplicationController
   before_action :login_requirement
   before_action :authenticate_user!, only: %i(new edit update destroy)
   before_action :set_task, only: %i(show edit update destroy)
+  before_action :correct_user?, only: %i(show edit destroy)
 
   def index
-    # @tasks = Task.all.order(created_at: :desc)
     @tasks = current_user.tasks.order(created_at: :desc)
 
     if params[:sort_expired]
-      # @tasks = Task.all.order(expires_on: :asc)
       @tasks = current_user.tasks.order(expires_on: :asc)
     else
-      # @tasks = Task.all.order(created_at: :desc)
-      @tasks = current_user.tasks.order(crated_at: :desc)
+      @tasks = current_user.tasks.order(created_at: :desc)
     end
 
     if params[:task].present?
@@ -67,6 +65,14 @@ class TasksController < ApplicationController
 
     def task_params
       params.require(:task).permit %i(title content expires_on status priority)
+    end
+
+    def correct_user?
+      @task = Task.find_by(id:params[:id])
+      if @task.user_id != current_user.id
+        flash[:notice] = t("view.task.no_rights")
+        redirect_to tasks_path
+      end
     end
 
 end
