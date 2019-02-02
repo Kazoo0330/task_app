d024f6eb14a28fe0e714b51f3d443acc9ebb3e81
KazukiSaito <kazoo@kazooPro.local>
2019-02-02 21:31:20 +0900

    :white_check_mark: admin user related feature spec and factories implemented.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,21 +1,22 @@
 FactoryBot.define do
 
-  # factory :user do
-  #   sequence(:name) { |n| "TEST_NAME_#{n}" }
-  #   # sequence(:email) { |n| "test#{n}@example.com" }
-  #   sequence(:email) { "test@example.com" }
-  #   sequence(:password) { "password" }
-  # end
-
-  # factory :user do
-  #   sequence(:name) { |n| "TEST_NAME#{n}"}
-  #   sequence(:email) { |n| "test#{n}@example.com"}
-  #   sequence(:password) { "password" }
-  # end
-
   factory :user do
     name { 'test_user' }
     email { 'test@example.com' }
     password { 'password' }
   end
+
+  factory :user2, class: User do
+    name { 'test_user2' }
+    email { 'test2@example.com' }
+    password { 'password' }
+  end
+
+  factory :admin_user, class: User do
+    name { 'admin_user' }
+    email { 'admin@example.com' }
+    admin { true }
+    password { 'password' }
+  end
+
 end
